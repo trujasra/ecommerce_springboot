afded0b41c524e2e8764583c54a2b3186094d792
Ramiro Trujillo <trujasra@gmail.com>
2023-03-17 20:02:03 -0400

    add application porperties profiles

--- src/main/java/com/postgrado/ecommerce/security/jwt/JwtService.java
+++ src/main/java/com/postgrado/ecommerce/security/jwt/JwtService.java
@@ -8,24 +8,28 @@ import com.postgrado.ecommerce.entity.User;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
-    public String generateToken(User user) {
-        return JWT.create()
-                .withSubject(user.getEmail())
-                .withClaim("id", user.getId().toString())
-                .withClaim("role", user.getRole().getName())
-                .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
-                .sign(Algorithm.HMAC256("myS3creyK3y"));
-    }
+  @Value("{spring.jwt.secret-key}")
+  private String secretKey;
 
-    public DecodedJWT decodeToken(String token) {
-        JWTVerifier verifier = JWT.require(Algorithm.HMAC256("myS3creyK3y")).build();
-        return verifier.verify(token);
-    }
+  public String generateToken(User user) {
+    return JWT.create()
+        .withSubject(user.getEmail())
+        .withClaim("id", user.getId().toString())
+        .withClaim("role", user.getRole().getName())
+        .withIssuedAt(new Date())
+        .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+        .sign(Algorithm.HMAC256(secretKey));
+  }
+
+  public DecodedJWT decodeToken(String token) {
+    JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secretKey)).build();
+    return verifier.verify(token);
+  }
 
 }
